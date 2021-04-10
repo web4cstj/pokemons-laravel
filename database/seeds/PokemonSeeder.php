daf5b1505978079b9fc437adb5b039dbb4edf897
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-04-10 13:41:05 -0400

    Coquille de slash

--- database/seeds/PokemonSeeder.php
+++ database/seeds/PokemonSeeder.php
@@ -11,6 +11,6 @@ class PokemonSeeder extends Seeder
      */
     public function run()
     {
-        DB::unprepared(file_get_contents(database_path('seeds\pokemon.sql')));
+        DB::unprepared(file_get_contents(database_path('seeds/pokemon.sql')));
     }
 }
